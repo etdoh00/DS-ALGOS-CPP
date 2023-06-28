5492129995c69bb9e2ca488ea271563595cc17b9
etdoh00 <129846755+etdoh00@users.noreply.github.com>
2023-06-28 15:04:41 +0100

    Update test.cpp

--- Pointers & Dynamic Allocation/test.cpp
+++ Pointers & Dynamic Allocation/test.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    // this uses a C style swap function where the values are accessed by reference. The function uses the "&" to view the value at the addresses for a and b. 
     int a = 0, b = 0;
     std::cout << "Please enter two numbers to swap : " << std::endl;
     std::cin >> a >> b;
@@ -12,6 +13,8 @@ int main()
 
     int c = 4, d = 5;
     int* pc = &c, *pd = &d;
+    //this uses the C++ pointer style version of the swap function. The variables pc and pd are pointers to the addresses of variables c and d.
+    //these pointers are then passed into the swap function and swap by using the pointers pointing to the values at the addresses at c and d 
     std::cout << "Numbers before swapping: " << "\nA: " << *pc << "\nB: " << *pd << std::endl;
     swap(*pc, *pd);
     std::cout << "Numbers after swapping: " << "\nA: " << *pc << "\nB: " << *pd << std::endl;
